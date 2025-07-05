bc28855bbaff3ee15475a3ff58e209d528548ea2
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-07-05 12:53:51 -0500

    change folder name

--- backend/server.js
+++ backend/server.js
@@ -269,9 +269,9 @@ app.get('/api/search',async (req,res)=>{
 
 if(process.env.NODE_ENV==='production'){
   
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'))
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
   })
 }
 
