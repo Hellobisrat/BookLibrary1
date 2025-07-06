b760eef2ead6a3e0d55627b53c01b3a91e081f01
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2025-07-06 12:08:05 -0500

    change api in bookStore

--- frontend/src/store/bookStore.js
+++ frontend/src/store/bookStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'https://booklibrary1-h0cq.onrender.com/api'
 
 axios.defaults.withCredentials = true;
 
